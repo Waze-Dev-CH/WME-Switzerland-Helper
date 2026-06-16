11d255124f69ab795c278ec652826c7e29807e22
Maël Pedretti <mael.pedretti@gmail.com>
2026-06-16 13:27:18 +0200

    Update URLs for update and download in header.js
    This prepares the transfer to the waze dev Switzerland organisation

--- header.js
+++ header.js
@@ -3,8 +3,8 @@
 // @namespace   wme-sdk-scripts
 // @version     1.3.0
 // @description WME Switzerland Helper is a userscript that provides various tools and enhancements for Waze Map Editor (WME) users in Switzerland.
-// @updateURL	  https://raw.githubusercontent.com/73VW/WME-Switzerland-Helper/releases/releases/main.user.js
-// @downloadURL https://raw.githubusercontent.com/73VW/WME-Switzerland-Helper/releases/releases/main.user.js
+// @updateURL	  https://raw.githubusercontent.com/Waze-Dev-CH/WME-Switzerland-Helper/releases/releases/main.user.js
+// @downloadURL https://raw.githubusercontent.com/Waze-Dev-CH/WME-Switzerland-Helper/releases/releases/main.user.js
 // @author      Maël Pedretti (Marelitaw)
 // @match       https://www.waze.com/editor*
 // @match       https://beta.waze.com/editor*
